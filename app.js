591935b3b37daeee4e9cc416cc3a215c7967e2f6
MadhushreeKunder <madhushreekunder@gmail.com>
2020-12-09 22:49:20 +0530

    minor changes

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 var buttonClicked = document.querySelector("#btn-click");
 var textInput = document.querySelector("#textinput");
+var outputDiv= document.querySelector("#output");
 
-console.log(textinput);
+// console.log(textinput);
 
+// outputDiv.innerText="Banana Translated";
 function clickHandler() {
      console.log("button is clicked!");
      console.log("input" , textinput.value);
 };
 
 buttonClicked.addEventListener("click", clickHandler);
-
-// 56.21
-
 
 
